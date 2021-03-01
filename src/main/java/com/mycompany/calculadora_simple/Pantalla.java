8bb5a1baf83ca8c450a96ccee65b264de22ed476
Mr Robot <qomc16@gmail.com>
2021-03-01 15:01:49 -0600

    Versión 7

--- src/main/java/com/mycompany/calculadora_simple/Pantalla.java
+++ src/main/java/com/mycompany/calculadora_simple/Pantalla.java
@@ -1,17 +1,26 @@
 package com.mycompany.calculadora_simple;
 //@author Fsociety
+
 public class Pantalla extends javax.swing.JFrame {
-private double numero1;
-private double numero2;
-private char operacion;
+
+    private double numero1;
+    private double numero2;
+    private char operacion;
+
     public Pantalla() {
         initComponents();
         setLocationRelativeTo(null);
-        
+
     }
-    
-    private void agregarTexto(String texto){
-        Ingreso.setText(Ingreso.getText()+texto);
+
+    private void agregarTexto(String texto) {
+        Ingreso.setText(Ingreso.getText() + texto);
+    }
+
+    private void seleccionarOperacion(char operacion) {
+        numero1 = Double.parseDouble(Ingreso.getText());
+        Ingreso.setText("");
+        this.operacion = operacion;
     }
 
     /**
@@ -71,7 +80,7 @@ private char operacion;
             }
         });
         getContentPane().add(jButton1);
-        jButton1.setBounds(40, 130, 50, 24);
+        jButton1.setBounds(40, 130, 50, 25);
 
         jButton4.setText("C");
         jButton4.addActionListener(new java.awt.event.ActionListener() {
@@ -80,7 +89,7 @@ private char operacion;
             }
         });
         getContentPane().add(jButton4);
-        jButton4.setBounds(200, 130, 50, 24);
+        jButton4.setBounds(200, 130, 50, 25);
 
         jButton5.setText("^1/2");
         jButton5.addActionListener(new java.awt.event.ActionListener() {
@@ -89,7 +98,7 @@ private char operacion;
             }
         });
         getContentPane().add(jButton5);
-        jButton5.setBounds(90, 130, 60, 24);
+        jButton5.setBounds(90, 130, 60, 25);
 
         jButton3.setText("CE");
         jButton3.addActionListener(new java.awt.event.ActionListener() {
@@ -98,7 +107,7 @@ private char operacion;
             }
         });
         getContentPane().add(jButton3);
-        jButton3.setBounds(150, 130, 50, 24);
+        jButton3.setBounds(150, 130, 50, 25);
 
         jButton6.setText("1");
         jButton6.addActionListener(new java.awt.event.ActionListener() {
@@ -107,7 +116,7 @@ private char operacion;
             }
         });
         getContentPane().add(jButton6);
-        jButton6.setBounds(30, 190, 50, 24);
+        jButton6.setBounds(30, 190, 50, 25);
 
         jButton7.setText("2");
         jButton7.addActionListener(new java.awt.event.ActionListener() {
@@ -116,7 +125,7 @@ private char operacion;
             }
         });
         getContentPane().add(jButton7);
-        jButton7.setBounds(80, 190, 50, 24);
+        jButton7.setBounds(80, 190, 50, 25);
 
         jButton8.setText("3");
         jButton8.addActionListener(new java.awt.event.ActionListener() {
@@ -125,7 +134,7 @@ private char operacion;
             }
         });
         getContentPane().add(jButton8);
-        jButton8.setBounds(130, 190, 50, 24);
+        jButton8.setBounds(130, 190, 50, 25);
 
         jButton9.setText("+");
         jButton9.addActionListener(new java.awt.event.ActionListener() {
@@ -134,11 +143,16 @@ private char operacion;
             }
         });
         getContentPane().add(jButton9);
-        jButton9.setBounds(180, 190, 40, 24);
+        jButton9.setBounds(180, 190, 50, 25);
 
         jButton10.setText("-");
+        jButton10.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton10ActionPerformed(evt);
+            }
+        });
         getContentPane().add(jButton10);
-        jButton10.setBounds(220, 190, 40, 24);
+        jButton10.setBounds(230, 190, 40, 25);
 
         jButton11.setText("4");
         jButton11.addActionListener(new java.awt.event.ActionListener() {
@@ -147,7 +161,7 @@ private char operacion;
             }
         });
         getContentPane().add(jButton11);
-        jButton11.setBounds(30, 230, 50, 24);
+        jButton11.setBounds(30, 230, 50, 25);
 
         jButton12.setText("5");
         jButton12.addActionListener(new java.awt.event.ActionListener() {
@@ -156,7 +170,7 @@ private char operacion;
             }
         });
         getContentPane().add(jButton12);
-        jButton12.setBounds(80, 230, 50, 24);
+        jButton12.setBounds(80, 230, 50, 25);
 
         jButton13.setText("6");
         jButton13.addActionListener(new java.awt.event.ActionListener() {
@@ -165,7 +179,7 @@ private char operacion;
             }
         });
         getContentPane().add(jButton13);
-        jButton13.setBounds(130, 230, 50, 24);
+        jButton13.setBounds(130, 230, 50, 25);
 
         jButton14.setText("*");
         jButton14.addActionListener(new java.awt.event.ActionListener() {
@@ -174,11 +188,16 @@ private char operacion;
             }
         });
         getContentPane().add(jButton14);
-        jButton14.setBounds(180, 230, 40, 24);
+        jButton14.setBounds(180, 230, 40, 25);
 
         jButton15.setText("/");
+        jButton15.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton15ActionPerformed(evt);
+            }
+        });
         getContentPane().add(jButton15);
-        jButton15.setBounds(220, 230, 40, 24);
+        jButton15.setBounds(220, 230, 40, 25);
 
         jButton16.setText(".");
         jButton16.setToolTipText("");
@@ -188,7 +207,7 @@ private char operacion;
             }
         });
         getContentPane().add(jButton16);
-        jButton16.setBounds(230, 270, 40, 24);
+        jButton16.setBounds(230, 270, 40, 25);
 
         jButton17.setText("8");
         jButton17.addActionListener(new java.awt.event.ActionListener() {
@@ -197,7 +216,7 @@ private char operacion;
             }
         });
         getContentPane().add(jButton17);
-        jButton17.setBounds(80, 270, 50, 24);
+        jButton17.setBounds(80, 270, 50, 25);
 
         jButton18.setText("9");
         jButton18.addActionListener(new java.awt.event.ActionListener() {
@@ -206,7 +225,7 @@ private char operacion;
             }
         });
         getContentPane().add(jButton18);
-        jButton18.setBounds(130, 270, 50, 24);
+        jButton18.setBounds(130, 270, 50, 25);
 
         jButton19.setText("0");
         jButton19.addActionListener(new java.awt.event.ActionListener() {
@@ -215,7 +234,7 @@ private char operacion;
             }
         });
         getContentPane().add(jButton19);
-        jButton19.setBounds(180, 270, 50, 24);
+        jButton19.setBounds(180, 270, 50, 25);
 
         jButton20.setText("7");
         jButton20.addActionListener(new java.awt.event.ActionListener() {
@@ -224,7 +243,7 @@ private char operacion;
             }
         });
         getContentPane().add(jButton20);
-        jButton20.setBounds(30, 270, 50, 24);
+        jButton20.setBounds(30, 270, 50, 25);
 
         jButton21.setText("+/-");
         jButton21.addActionListener(new java.awt.event.ActionListener() {
@@ -233,11 +252,16 @@ private char operacion;
             }
         });
         getContentPane().add(jButton21);
-        jButton21.setBounds(90, 340, 50, 24);
+        jButton21.setBounds(90, 340, 50, 25);
 
         jButton22.setText("=");
+        jButton22.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton22ActionPerformed(evt);
+            }
+        });
         getContentPane().add(jButton22);
-        jButton22.setBounds(140, 340, 79, 24);
+        jButton22.setBounds(140, 340, 79, 25);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -247,15 +271,19 @@ private char operacion;
     }//GEN-LAST:event_IngresoActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+        //Limpiar Pantalla
+        Ingreso.setText("");
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton9ActionPerformed
-        // TODO add your handling code here:
+        // Boton suma
+        seleccionarOperacion('+');
     }//GEN-LAST:event_jButton9ActionPerformed
 
     private void jButton14ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton14ActionPerformed
-        // TODO add your handling code here:
+        // Boton Multiplicación
+        seleccionarOperacion('*');
+
     }//GEN-LAST:event_jButton14ActionPerformed
 
     private void jButton19ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton19ActionPerformed
@@ -264,20 +292,24 @@ private char operacion;
     }//GEN-LAST:event_jButton19ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        numero1=Double.parseDouble(Ingreso.getText());
-        numero1 *=numero1;
+        numero1 = Double.parseDouble(Ingreso.getText());
+        numero1 *= numero1;
         Ingreso.setText(String.valueOf(numero1));
-        
+
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
-       numero1=Double.parseDouble(Ingreso.getText());
-        numero1 =Math.sqrt(numero1);
+        numero1 = Double.parseDouble(Ingreso.getText());
+        numero1 = Math.sqrt(numero1);
         Ingreso.setText(String.valueOf(numero1));
     }//GEN-LAST:event_jButton5ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        // TODO add your handling code here:
+        // Limpiar todo
+        numero1=0;
+        numero2=0;
+        operacion=' ';
+        Ingreso.setText(" ");
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
@@ -332,10 +364,40 @@ private char operacion;
 
     private void jButton21ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton21ActionPerformed
         //Boton
-        Ingreso.setText("-"+Ingreso.getText());
+        Ingreso.setText("-" + Ingreso.getText());
+
     }//GEN-LAST:event_jButton21ActionPerformed
 
-   
+    private void jButton10ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton10ActionPerformed
+        // Resta
+        seleccionarOperacion('-');
+    }//GEN-LAST:event_jButton10ActionPerformed
+
+    private void jButton15ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton15ActionPerformed
+        //Botón División
+        seleccionarOperacion('/');
+    }//GEN-LAST:event_jButton15ActionPerformed
+
+    private void jButton22ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton22ActionPerformed
+        //Boton Igual 
+        numero2 = Double.parseDouble(Ingreso.getText());
+        switch (operacion) {
+            case '+':
+                numero2 = numero1 + numero2;
+                break;
+            case '-':
+                numero2 = numero1 - numero2;
+                break;
+            case '*':
+                numero2 = numero1 * numero2;
+                break;
+            case '/':
+                numero2 = numero1 / numero2;
+                break;
+        }
+        Ingreso.setText(String.valueOf(numero2));
+    }//GEN-LAST:event_jButton22ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField Ingreso;
